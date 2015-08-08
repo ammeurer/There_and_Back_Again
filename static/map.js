3aa2f05eaf818793447a07442eaa2230da3e3206
ammeurer <ammeurer8@gmail.com>
2015-08-07 17:33:43 -0700

    Adding user profile stuff this time

---
+++ static/map.js
@@ -0,0 +1,28 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiYW1tZXVyZXIiLCJhIjoiMjg1M2JlM2QwMmE2NjIzZjJmODRjYWM1NDRjODg5YWEifQ.SZQ4FO0OqKTTE4tiv4Au4A';
+var map = L.mapbox.map('map', 'ammeurer.5d704af1', {
+    zoomControl: true
+}).setView([37.78, -122.416], 13);
+
+// move the attribution control out of the way
+map.attributionControl.setPosition('bottomleft');
+
+// create the initial directions object, from which the layer
+// and inputs will pull data.
+var directions = L.mapbox.directions({
+    profile: 'mapbox.walking'
+});
+console.log(directions);
+var directionsLayer = L.mapbox.directions.layer(directions)
+    .addTo(map);
+
+var directionsInputControl = L.mapbox.directions.inputControl('inputs', directions)
+    .addTo(map);
+
+var directionsErrorsControl = L.mapbox.directions.errorsControl('errors', directions)
+    .addTo(map);
+
+var directionsRoutesControl = L.mapbox.directions.routesControl('routes', directions)
+    .addTo(map);
+
+var directionsInstructionsControl = L.mapbox.directions.instructionsControl('instructions', directions)
+    .addTo(map);
